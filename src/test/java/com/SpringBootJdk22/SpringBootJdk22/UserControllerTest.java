04be5759cd84809303709c926fcd1f3845917afd
DuyLuan <luanml0016@gmail.com>
2025-03-12 01:14:09 +0700

    Junit

--- src/test/java/com/SpringBootJdk22/SpringBootJdk22/UserControllerTest.java
+++ src/test/java/com/SpringBootJdk22/SpringBootJdk22/UserControllerTest.java
@@ -6,10 +6,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+import com.SpringBootJdk22.SpringBootJdk22.model.User;
+import com.SpringBootJdk22.SpringBootJdk22.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,7 +24,18 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @BeforeEach
+    public void setup() {
+        User user = new User();
+        user.setUsername("user");
+        user.setPassword(passwordEncoder.encode("password")); // Mã hóa password
+        userRepository.save(user);
+    }
     @Test
     public void testShowLoginPage() throws Exception {
         mockMvc.perform(get("/login")) // Gửi yêu cầu GET đến /login
